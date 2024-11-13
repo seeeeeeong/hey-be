65791b1a19585cf3db1410a296c3c7b655002fa0
seeeeeeong <lsinsung@gmail.com>
2024-11-13 22:33:19 +0900

    refactor: response

--- src/main/java/hey/io/heybackend/domain/artist/controller/ArtistController.java
+++ src/main/java/hey/io/heybackend/domain/artist/controller/ArtistController.java
@@ -4,6 +4,7 @@ import hey.io.heybackend.common.exception.ErrorCode;
 import hey.io.heybackend.common.response.ApiResponse;
 import hey.io.heybackend.common.resolver.AuthUser;
 import hey.io.heybackend.common.swagger.ApiErrorCode;
+import hey.io.heybackend.common.swagger.ApiErrorCodes;
 import hey.io.heybackend.domain.artist.dto.ArtistDetailResponse;
 import hey.io.heybackend.domain.artist.service.ArtistService;
 import hey.io.heybackend.domain.system.dto.TokenDTO;
@@ -29,7 +30,7 @@ public class ArtistController {
      */
     @Operation(summary = "아티스트 상세", description = "아티스트 상세 정보를 조회합니다.")
     @GetMapping("/{id}")
-    @ApiErrorCode(ErrorCode.ARTIST_NOT_FOUND)
+    @ApiErrorCodes({ErrorCode.MEMBER_NOT_FOUND, ErrorCode.ARTIST_NOT_FOUND})
     public ApiResponse<ArtistDetailResponse> getArtistDetail(@PathVariable("id") Long artistId,
                                                              @AuthUser @Parameter(hidden = true) TokenDTO tokenDTO) {
         return ApiResponse.success(artistService.getArtistDetail(artistId, tokenDTO));
